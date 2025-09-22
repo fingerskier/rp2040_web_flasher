ece6f8c626fd46a1dce51d9d60c8ef840aa5cb17
Matt Currier <fingerskier@gmail.com>
2025-09-22 17:41:27 -0500

    Refactor Header component to include logo image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Header from './com/Header'
-import Main from './com/Main'
-import Footer from './com/Footer'
-import { DeviceProvider } from './lib/DeviceContext'
+import Header from '@/com/Header'
+import Main from '@/com/Main'
+import Footer from '@/com/Footer'
+import { DeviceProvider } from '@/lib/DeviceContext'
 
 import './App.css'
+
 
 export default function App() {
   return (
--- src/com/Header.jsx
+++ src/com/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import logo from '@/assets/logo.svg'
+
+
 export default function Header() {
   return <header>
-    <h1>RP2040 Web Flasher</h1>
+    <h1>
+      <img src={logo} className="logo" alt="logo" />
+      RP2040 Web Flasher
+    </h1>
   </header>
 }
