06d91661c51cfa6e5e95ad6e05869b257c484bcb
Matt Currier <fingerskier@gmail.com>
2025-09-22 17:09:40 -0500

    Set baseURL in Vite configuration for consistent deployment path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  baseURL: '/rp2040_web_flasher',
   plugins: [react()],
   resolve: {
     alias: {
